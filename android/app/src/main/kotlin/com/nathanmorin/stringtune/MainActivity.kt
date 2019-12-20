63dcfacf0c3928023a2a75fec2a8911ed8eac9ed
Nathan Morin <nathanamorin@gmail.com>
2019-12-20 18:18:06 -0500

    cleanup android

--- android/app/src/main/kotlin/com/nathanmorin/stringtune/MainActivity.kt
+++ android/app/src/main/kotlin/com/nathanmorin/stringtune/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.untitled
+package com.nathanmorin.stringtune
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'tune_page.dart';
 
 import 'instruments.dart';
