b611cdc03dc4a201f25d1794452a71ea4173874f
Nathan Morin <nathanamorin@gmail.com>
2019-12-20 16:26:44 -0500

    add fav

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,45 +1,187 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled/tune_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'tune_page.dart';
 
 import 'instruments.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new _HomePage();
+  }
+}
+
+class _HomePage extends State<HomePage> {
+  String currentQuery = "";
+  List<Instrument> instruments = new List();
+  List<Instrument> displayedInstruments = new List();
+  TextEditingController editingController = TextEditingController();
+  static final String savedInstrumentsKey = "saved_instruments";
+
+  void refreshListView() {
+    if (currentQuery.isNotEmpty) {
+        displayedInstruments.clear();
+        instruments.forEach((instrument) {
+          if (instrument.name.toLowerCase().contains(currentQuery.toLowerCase())) {
+            displayedInstruments.add(instrument);
+          }
+        });
+    } else {
+        displayedInstruments.clear();
+        displayedInstruments.addAll(instruments);
+    }
+  }
+
+  List<String> getSavedList(SharedPreferences prefs, String key) {
+    List<String> l = prefs.getStringList(key);
+    if (l == null) {
+      return new List<String>();
+    } else {
+      return l;
+    }
+  }
+
+  void filterSearch(String query) {
+    setState(() {
+      currentQuery = query;
+      refreshListView();
+    });
+  }
+
+  Future<void> storeSavedInstrument(Instrument instrument) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    List<String> savedInstruments = getSavedList(prefs, savedInstrumentsKey);
+    savedInstruments.add(instrument.getId());
+
+    prefs.setStringList(savedInstrumentsKey, savedInstruments);
+
+    setState(() {
+      loadInstruments(savedInstruments);
+    });
+
+  }
+
+  Future<void> removeSavedInstrument(Instrument instrument) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    List<String> savedInstruments = getSavedList(prefs, savedInstrumentsKey);
+    savedInstruments.remove(instrument.getId());
+
+    prefs.setStringList(savedInstrumentsKey, savedInstruments);
+
+
+    setState(() {
+      loadInstruments(savedInstruments);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("String Tuner"),),
-      body: FutureBuilder<String>(
-        future: loadCSV(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            List<Instrument> instruments = parseCSV(snapshot.data);
-            return new ListView.builder(
-                itemCount: instruments.length,
-                itemBuilder: (context, rowNum) {
-                  return InkWell(
-                    child: new Container(
-                      child: new Text(
-                          instruments[rowNum].name.toString()),
+        appBar: new AppBar(
+          title: new Text("String Tuner"),
+        ),
+        body: Column(children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Theme(
+                data: Theme.of(context)
+                    .copyWith(dialogBackgroundColor: Colors.transparent),
+                child: TextField(
+                  onChanged: (value) {
+                    filterSearch(value);
+                  },
+                  controller: editingController,
+                  decoration: InputDecoration(
+                      labelText: "Search",
+                      hintText: "Search",
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(25.0))),
+                      fillColor: Colors.transparent),
+                ),
+              )),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: displayedInstruments.length,
+                  itemBuilder: (context, rowNum) {
+                    Instrument instrument = displayedInstruments[rowNum];
+
+                    return Container(
+                      child: Row(children: <Widget>[
+                        Expanded(
+                            flex: 2,
+                            child: InkWell(
+                              child: Icon(
+                                Icons.star,
+                                color: instrument.starred ? Colors.yellow : Colors.white ,
+                                size: 50,
+                              ),
+                              onTap: () {
+                                if (instrument.starred) {
+                                  removeSavedInstrument(instrument);
+                                } else {
+                                  storeSavedInstrument(instrument);
+                                }
+                              },
+                            )),
+                        Expanded(
+                            flex: 8,
+                            child: InkWell(
+                              child: FittedBox(
+                                  fit: BoxFit.fitHeight,
+                                  child: Text(instrument.name.toString(),
+                                      style: TextStyle(fontSize: 50))),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          TunePage(instrument)),
+                                );
+                              },
+                            )),
+                      ]),
                       color: Colors.indigoAccent,
                       margin: new EdgeInsets.all(10.0),
                       padding: new EdgeInsets.all(10.0),
                       alignment: Alignment.center,
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => TunePage(instruments[rowNum].notes)),
-                      );
-                    },
-                  );
-                });
-          } else {
-            return Text("Loading");
-          }
-        },
-      ),
-    );
+                    );
+                  }))
+        ]));
+  }
+
+  void loadInstruments(List<String> savedInstruments) {
+    instruments.forEach((instrument) {
+      instrument.starred = savedInstruments.contains(instrument.getId());
+    });
+
+    instruments.sort((a, b) {
+      if (a.starred && !b.starred) {
+        return -1;
+      } else if (!a.starred && b.starred){
+        return 1;
+      } else {
+        return a.name.compareTo(b.name);
+      }
+    });
+    refreshListView();
+  }
+
+  @override
+  void initState() {
+    instruments.addAll(instrumentsStore);
+
+    // Uncomment SharedPreferences & Remove refreshListView after web issue is fixed
+//    SharedPreferences.getInstance().then((prefs) {
+//      List<String> savedInstruments = getSavedList(prefs, savedInstrumentsKey);
+//      loadInstruments(savedInstruments);
+//    });
+    refreshListView();
+    super.initState();
   }
 }
--- lib/tune_page.dart
+++ lib/tune_page.dart
@@ -1,4 +1,3 @@
-
 import 'dart:typed_data';
 
 import 'package:flutter/cupertino.dart';
@@ -7,45 +6,106 @@ import 'package:flutter/services.dart';
 import 'package:soundpool/soundpool.dart';
 import 'package:wave_generator/wave_generator.dart';
 
+import 'instruments.dart';
+
+Map<String,String> unicodeSubscriptMap = {
+  '0'        : '\u2080',
+  '1'        : '\u2081',
+  '2'        : '\u2082',
+  '3'        : '\u2083',
+  '4'        : '\u2084',
+  '5'        : '\u2085',
+  '6'        : '\u2086',
+  '7'        : '\u2087',
+  '8'        : '\u2088',
+  '9'        : '\u2089',
+};
 
 Future<void> playTone(note) async {
+  Soundpool pool = Soundpool(streamType: StreamType.notification);
 
-    Soundpool pool = Soundpool(streamType: StreamType.notification);
-
-    int soundId = await rootBundle.load("data/tones/" + note + ".wav").then((ByteData soundData) {
-      return pool.load(soundData);
-    });
-    int streamId = await pool.play(soundId);
+  int soundId = await rootBundle
+      .load("data/tones/" + note + ".wav")
+      .then((ByteData soundData) {
+    return pool.load(soundData);
+  });
+  int streamId = await pool.play(soundId);
 }
 
 class TunePage extends StatelessWidget {
-  final String notes;
-  TunePage(this.notes);
+  final Instrument instrument;
+  TunePage(this.instrument);
 
   @override
   Widget build(BuildContext context) {
-
-    List<String> notesSplit = notes.split(":");
+    List<String> notesSplit = instrument.notes.split(":");
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("String Tuner"),),
-      body: GridView.count(crossAxisCount: 2,
+        title: new Text("Tune " + instrument.name),
+      ),
+      body: GridView.count(
+          crossAxisCount: 2,
           children: List.generate(notesSplit.length, (i) {
-              return FlatButton(
-                  onPressed: () {
-                    playTone(notesSplit[i]).then((val) {
-                      print("finished tone");
-                    });
-                  },
-                  child: new Container(
-                      child: new Text(notesSplit[i]),
-                      color: Colors.teal,
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.all(10.0),
-                      padding: EdgeInsets.all(10.0),
+            return InkWell(
+              onTap: () {
+                playTone(notesSplit[i]).then((val) {
+                  print("finished tone");
+                });
+              },
+              child: new Container(
+                child: FittedBox(
+                  fit: BoxFit.fitHeight,
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(text: notesSplit[i][0], style: TextStyle(fontSize: 500, color: Colors.white)),
+                        TextSpan(text: unicodeSubscriptMap[notesSplit[i][1]], style: TextStyle(fontSize: 500, color: Colors.black))
+                      ]
                     )
-              );
+                  ),
+                ),
+                color: Colors.indigoAccent,
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10.0),
+              ),
+              enableFeedback: false,
+            );
           })),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
